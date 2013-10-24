c6e02262e587765072d0c922c21e2149bb50d0e4
刘当一 <LeedyPKU@gmail.com>
2013-10-24 12:58:07 +0800

    production

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/bdfz/code/peer/"
+pid "/home/bdfz/code/peer/tmp/pids/unicorn.pid"
+stderr_path "/home/bdfz/code/peer/log/unicorn.log"
+stdout_path "/home/bdfz/code/peer/log/unicorn.log"
+
+listen "/tmp/unicorn.peer.sock"
+worker_processes 4
+timeout 30
