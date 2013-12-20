45ab0a5cf7af29f1ddf6354b90025c6f1108e38d
Cassius Chen <chzsh1995@gmail.com>
2013-12-20 13:57:13 +0800

    clean up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,17 +41,10 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-<<<<<<< HEAD
-    if current_user.is_create_post? && is_post_submit?
-      @post = Post.new
-    else
-      redirect_to :back, alert: { danger: "新建时发生了一个错误，我们将在或长或短的时间里修复这个问题!" }
-=======
     if current_user.can_create_post? && is_post_submit?
       @post = Post.new
     else
-      redirect_to :back, alert: { danger: "New error!" }
->>>>>>> release/ver.0.2.2
+      redirect_to :back, alert: { danger: "新建时发生了一个错误，我们将在或长或短的时间里修复这个问题!" }
     end
   end
 
@@ -61,11 +54,7 @@ class PostsController < ApplicationController
 
   # POST /posts
   def create
-<<<<<<< HEAD
-    if current_user.is_create_post? && is_post_submit?
-=======
     if current_user.can_create_post? && is_post_submit?
->>>>>>> release/ver.0.2.2
       @post = Post.new(post_params)
       @post.author = current_user
 
@@ -75,11 +64,7 @@ class PostsController < ApplicationController
         render action: 'new'
       end
     else
-<<<<<<< HEAD
       redirect_to :back, alert: { danger: "新建时发生了一个错误，我们将在或长或短的时间里修复这个问题!" }
-=======
-      redirect_to :back, alert: { danger: "New error!" }
->>>>>>> release/ver.0.2.2
     end
   end
 
